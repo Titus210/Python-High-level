ab7675366eec254ec4477721fbc940dbd99b6bb8
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-10-04 08:52:00 +0300

    Pizza needed

---
+++ 0-numbers/7-pizza_needed.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+	How many pizza needed:
+		input:
+			number of people
+			number of slices per pie
+			number of slices each person will eat
+		output:
+			number of pizza needed
+"""
+people = int(input("Enter number of people: "))
+slices_person = int(input("How many alices will each person eat: "))
+slices = int(input("Enter slices in each pie: "))
+def slices_needed(people,slices,pie):
+	pizza = people * slices
+	no_needed = pizza / pie
+	print(f"The number of pizza needed is {no_needed}")
+
+slices_needed(people,slices_person,slices)
