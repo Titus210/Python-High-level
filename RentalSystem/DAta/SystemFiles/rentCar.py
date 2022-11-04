96fcf63c7659ee44fe52b98620a0a03b90a64022
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-04 15:05:28 +0300

    VAlidate year

--- RentalSystem/DAta/SystemFiles/rentCar.py
+++ RentalSystem/DAta/SystemFiles/rentCar.py
@@ -3,6 +3,9 @@
 #---------------------------------------
 #          Car Details    
 #---------------------------------------
+from datetime import datetime
+import re
+import math
 def car_to_rent():
     # Ask registration number of car to rent
     regNumber = str(input("Please enter Registration Number of Car"))
@@ -20,13 +23,12 @@ def car_to_rent():
             for cars in f:
                 regRow = str(cars.split(",")[0])
                 regVehicle += [regRow]
-        print(regVehicle)
         if regNumber in regVehicle:
             print("Car is Rented")
         else:
-            print("Car Availabile")
+            validate_Birthday()
     else:
-        print("Car in the system")
+        print("Car not in the system")
 
 
 
@@ -35,6 +37,8 @@ def car_to_rent():
 #------------------------------------------------
 
 # Validate Date
+import math
+import re
 import time
 def validate_Birthday():
     Result = None
@@ -44,21 +48,38 @@ def validate_Birthday():
         # Check if birthday is given in right format (DD/MM/YYY)
         try:
             date = time.strptime(bDay, "%d/%m/%Y")
-            print(f"Your Birthday is: {bDay}")
             result = True
             break
         except ValueError:
             print(f"Sorry!, {bDay} is invalid date!")
             result = False
             continue
-    return result
+
+        # Check If date and year is valid (dd < 31, mm <= 12, yyyy <= 2022)
+    if result == True:
+        regex = re.search(
+            r'^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$', bDay)
+        if regex:
+            striped_time = datetime.strptime(bDay, '%m/%d/%Y')
+            age = ((datetime.today() - striped_time).days / 365)
+
+        # Check if age is <= 100  and age is >= 18
+            if age >= 18 and age <= 100:
+                print("Eligible")
+
+            else:
+                print("Not Eligible!")
+                validate_Birthday()
+        else:
+            print("Enter A vald date format")
+            validate_Birthday()
+
+
+car_to_rent()
 
 
 
 
-# Date is sensible (Date 30/31 days and month 12) 
-
-# Check if age range ( less than 100) and (greater or equal to 18 from 2022)
 
 
 """      Customers Availability  """
