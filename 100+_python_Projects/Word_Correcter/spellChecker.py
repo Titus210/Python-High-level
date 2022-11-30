d7afedc549bd07c6df1e7a99b7f2228bd8428e8e
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-30 21:23:59 +0300

    mod

--- 100+_python_Projects/Word_Correcter/spellChecker.py
+++ 100+_python_Projects/Word_Correcter/spellChecker.py
@@ -2,10 +2,16 @@ from spellchecker import SpellChecker
 
 corrector = SpellChecker()
 
-word = input("Enter a word: ")
-if word == corrector:
-    print("This is a valid word")
-else:
-    correct_word = corrector.correction(word)
-    print(f'The correct spelling of {word} is {correct_word}')
+while True:
+    try:
+        word = str(input("Enter a word: "))
+    except ValueError:
+        print("Please enter a valid word:")
 
+    if word in corrector:
+        print("This is a valid word")
+        break
+    else:
+        correct_word = corrector.correction(word)
+        print(f'The correct spelling of {word} is {correct_word}')
+    break
