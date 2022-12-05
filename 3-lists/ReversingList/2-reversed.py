4e207bd6945731b2a6fe6d0dc6124452d3f6dd24
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-12-06 01:15:03 +0300

    reversed(list) Change back to list after reverse

---
+++ 3-lists/ReversingList/2-reversed.py
@@ -0,0 +1,7 @@
+
+
+original_list = [1, 2, 4, 7, 8, 10]
+print(f"List before reverse: {original_list}")
+reversed_list = reversed(original_list)
+print(f"List after reverse: {list(reversed_list)}")
+ 
