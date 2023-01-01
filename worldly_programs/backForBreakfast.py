b9cd4245543635c6281bed0e0ace20da126a455e
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-01-02 00:51:46 +0300

    Math Calculate breakfast time

---
+++ worldly_programs/backForBreakfast.py
@@ -0,0 +1,3 @@
+# If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at
+# tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
+-
