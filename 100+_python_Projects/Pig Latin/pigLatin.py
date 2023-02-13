7bfc59459e05cbed4ee3692929d9dedc0d5e9683
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-02-13 22:02:18 +0300

    Pig latin

--- 100+_python_Projects/Pig Latin/pigLatin.py
+++ 100+_python_Projects/Pig Latin/pigLatin.py
@@ -5,23 +5,29 @@
 def pigLatin():
     """
     """
-    
 
     begin_with_vowel = False
-    
-    # enter word
+
     word = str(input("Enter a word: "))
-    vowels = ["a","e","i","o","u"]
-    
+    vowels = ["a", "e", "i", "o", "u"]
+
+    # check if word begins with vowel
     for vowel in vowels:
         if word[0] == vowel:
             begin_with_vowel = True
-    
-    if  begin_with_vowel ==  False:
-        print(f"{word} does not begin with vowel")
+
+    if begin_with_vowel == False:
+        # add way to end of word
+        # get first word
+        first_word = word[0]
+        first_word_end = first_word + "ay"
+
+        new_word = word + first_word_end
+        print(f"{word} ends with and added way is {new_word}")
     else:
-        print(f"{word} begins with vowel")
-        
-        
+        last_word = word + "way"
+
+        print(f"{word} ends with and added way is {last_word}")
+
 
 pigLatin()
