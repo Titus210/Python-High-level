550183e75863235d4d507d75a7858812c11736f6
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-02-26 17:24:48 +0300

    Pig Latin

--- 100+_python_Projects/Pig Latin/simplifiedVersion.py
+++ 100+_python_Projects/Pig Latin/simplifiedVersion.py
@@ -1,3 +1,9 @@
+import time
+
+
+
+
+
 def pig_latin(word):
     """Pig latin to convert english words to pig latin
 
@@ -10,10 +16,21 @@ def pig_latin(word):
     return f'{word[1:]}{word[0]}ay'
 
 
+# get starting time to run the code
+start_time = time.perf_counter()
+
 try:
     word = str(input("enter a word to translate to pig latin: "))
 except ValueError:
     print("Enter a string value: ")
 
-
 print(pig_latin(word))
+
+# get starting time to run the code
+end_time = time.perf_counter()
+time_taken = end_time - start_time
+
+print(f"The total time taken is {time_taken:.2f} seconds")
+
+
+
