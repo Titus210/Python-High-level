74bcf1b0e25f6c2e12a216aa5663fb2a0ae66d4c
Titus210 <tituskiplagat210@gmail.com>
2023-06-17 15:14:35 +0300

    reversing an object pytho n

--- 3-lists/0-reverse_list.py
+++ 3-lists/0-reverse_list.py
@@ -2,3 +2,8 @@
 my_rev = ["apple","an", " ate" ,"Titus"]
 my_rev.reverse()
 print(my_rev)
+
+
+reverse_school = ['study', 'to','school','to' ,'went', 'I']
+reverse_school.reverse()
+print(reverse_school)
