62da2ef49915d4d4c3cb855f47d7685aeca8bcf9
Titus210 <tituskiplagat210@gmail.com>
2023-06-17 22:59:06 +0300

    storing images in django application

--- DJANGO/tshirt/tshirt_eccom/tshirt_eccom/settings.py
+++ DJANGO/tshirt/tshirt_eccom/tshirt_eccom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/ '
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
