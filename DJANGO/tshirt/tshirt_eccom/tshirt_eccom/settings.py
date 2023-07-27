eb425bbf7c5b5103188c354788f4d2e95cb6be6b
Titus210 <tituskiplagat210@gmail.com>
2023-07-27 10:15:26 +0300

    feat; User views and settings

--- DJANGO/tshirt/tshirt_eccom/tshirt_eccom/settings.py
+++ DJANGO/tshirt/tshirt_eccom/tshirt_eccom/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "corsheaders",
     'rest_framework',
     'rest_framework.authtoken',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/ '
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
