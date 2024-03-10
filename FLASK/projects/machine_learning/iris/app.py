ac4e2426b49bbcffefe3e5879759df382f4e09a7
Titus210 <tituskiplagat210@gmail.com>
2024-03-10 11:49:23 +0300

    feat: Preparing for production chnaged debug to False

--- FLASK/projects/machine_learning/iris/app.py
+++ FLASK/projects/machine_learning/iris/app.py
@@ -5,15 +5,19 @@ from flask import Flask, request, jsonify, render_template
 # create an instance of the Flask class
 app = Flask(__name__)
 
-# Load pickle model 
+# Load pickle model
 model = pickle.load(open('./assets/model.pkl', 'rb'))
 
 # Create Homepage
+
+
 @app.route('/')
 def Home():
     return render_template('index.html')
 
 # API endpoint
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     float_features = [float(x) for x in request.form.values()]
@@ -21,5 +25,6 @@ def predict():
     prediction = model.predict(features)
     return render_template('index.html', prediction_text='The species of the iris flower is {}'.format(prediction))
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
