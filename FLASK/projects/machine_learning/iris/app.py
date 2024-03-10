00976994ea0672e3425fb2e47c01829f400e1990
Titus210 <tituskiplagat210@gmail.com>
2024-03-10 11:43:46 +0300

    feat: Implemented Login page and styled - Ceated an interface for the pickle model

---
+++ FLASK/projects/machine_learning/iris/app.py
@@ -0,0 +1,25 @@
+import pickle
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+# create an instance of the Flask class
+app = Flask(__name__)
+
+# Load pickle model 
+model = pickle.load(open('./assets/model.pkl', 'rb'))
+
+# Create Homepage
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+# API endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template('index.html', prediction_text='The species of the iris flower is {}'.format(prediction))
+
+if __name__ == '__main__':
+    app.run(debug=True)
